0355c626af21a32624d2eb3041fa95891daf50c7
vladandrei51 <vladandrei51@gmail.com>
2025-05-06 10:36:27 -0500

    Initial Commit

---
+++ ios-whereabouts/ios_whereaboutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_whereaboutsApp.swift
+//  ios-whereabouts
+//
+//  Created by Andrei on 06.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ios_whereaboutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-whereaboutsTests/ios_whereaboutsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_whereaboutsTests.swift
+//  ios-whereaboutsTests
+//
+//  Created by Andrei on 06.05.2025.
+//
+
+import Testing
+@testable import ios_whereabouts
+
+struct ios_whereaboutsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
